88539e6d68271b2fd7fd76ffee8da280db2415c6
Daniel Giustini Perez <danielgiustiniperez@gmail.com>
2021-07-29 08:33:47 +0200

    add base styles and typography

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <h1>Hello World</h1>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@font-face {
+   font-family: "Oswald";
+   src: url("./fonts/oswald-bold.woff2") format("woff2"),
+      url("./fonts/oswald-bold.woff") format("woff");
+   font-weight: 700;
+   font-style: normal;
+   font-display: swap;
+}
+
+@font-face {
+   font-family: "Oswald";
+   src: url("./fonts/oswald-regular.woff2") format("woff2"),
+      url("./fonts/oswald-regular.woff") format("woff");
+   font-weight: 400;
+   font-style: normal;
+   font-display: swap;
+}
+
+*,
+*::before,
+*::after {
+   margin: 0;
+   padding: 0;
+   box-sizing: inherit;
+}
+
+html {
+   font: 400 100%/1 "Oswald", sans-serif;
+}
+
+body {
+   box-sizing: inherit;
+}
+
+button,
+input {
+   outline: none;
 }
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1 class="heading-1">Learning Resources App</h1>
+</template>
+
+<style lang="scss" scoped>
+.heading-1 {
+    
+}
+</style>
